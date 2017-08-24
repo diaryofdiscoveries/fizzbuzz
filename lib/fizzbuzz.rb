8fe17b7c56e99de083eda452b7609883e1f9d811
zoe hopkins <zoe.hopkins@gmail.com>
2017-08-24 15:41:40 +0000

    Update fizzbuzz.rb to print number if not a multiple of 3 or 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,5 +2,6 @@ def fizzbuzz(number)
     string = ""
     string << "fizz" if number % 3 == 0
     string << "buzz" if number % 5 == 0
+    string << "#{number}" if number % 3 != 0 && number % 5 != 0
     string
 end
