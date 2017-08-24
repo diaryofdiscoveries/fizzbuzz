a82259bc294d83a43d284cca3246b7d3b5736092
Aga Lis <agnieszkalis@gmail.com>
2017-08-24 15:32:43 +0000

    Updating fizzbuzz_spec.rb with test for numbers that are not multiples of 3 or 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
 	it 'returns "buzz" when passed 5' do
 		expect(fizzbuzz(5)).to eq 'buzz'
 	end
+	it "returns number when it's not a multiple of 3 or 5" do
+		expect(fizzbuzz(1)).to eq '1'
+	end
 end
